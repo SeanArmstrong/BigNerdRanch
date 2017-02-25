0ea77800c3b2194de72add1279b325e50d3e2d32
Sean Armstrong <sean.armstrong@sage.com>
2017-02-25 18:15:02 +0000

    end chapter 6

--- GeoQuiz/app/src/main/java/com/example/seanarmstrong/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/seanarmstrong/geoquiz/CheatActivity.java
@@ -1,14 +1,17 @@
 package com.example.seanarmstrong.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.text.TextDirectionHeuristicCompat;
-import android.support.v4.widget.TextViewCompat;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class CheatActivity extends AppCompatActivity {
 
@@ -17,7 +20,8 @@ public class CheatActivity extends AppCompatActivity {
 
 
     private boolean mAnswerIsTrue;
-    private TextView mAnswerTextview;
+    private TextView mAnswerTextView;
+    private TextView mAndroidVersion;
     private Button mShowAnswerButton;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
@@ -37,20 +41,41 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
-        mAnswerTextview = (TextView) findViewById(R.id.answer_text_view);
-        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
-                    mAnswerTextview.setText(R.string.true_button);
+                    mAnswerTextView.setText(R.string.true_button);
                 } else {
-                    mAnswerTextview.setText(R.string.false_button);
+                    mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        mAndroidVersion = (TextView) findViewById(R.id.android_version);
+        mAndroidVersion.setText("API Level " + Build.VERSION.SDK_INT);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
